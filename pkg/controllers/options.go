ebe72a9ecefb2106122b2efea7853a6dc024a5de
Nick Mills-Barrett <nick@beeper.com>
2024-02-21 14:44:31 +0000

    Add `CATTLE_GLOBAL_LABEL_SELECTOR` env var
    This enables setting a label selector on all list/watch calls through
    the shared informer cache/controllers.
    
    # Conflicts:
    #	pkg/controllers/options.go

--- pkg/controllers/options.go
+++ pkg/controllers/options.go
@@ -7,7 +7,9 @@ import (
 	"os"
 	"strings"
 
+	"github.com/rancher/lasso/pkg/cache"
 	"github.com/rancher/lasso/pkg/controller"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/rest"
 )
 
@@ -23,6 +25,9 @@ const (
 func GetOptsFromEnv(contextType controllerContextType) *controller.SharedControllerFactoryOptions {
 	return &controller.SharedControllerFactoryOptions{
 		SyncOnlyChangedObjects: syncOnlyChangedObjects(contextType),
+		CacheOptions: &cache.SharedCacheFactoryOptions{
+			DefaultTweakList: defaultTweakListOptions(),
+		},
 	}
 }
 
@@ -41,6 +46,16 @@ func syncOnlyChangedObjects(option controllerContextType) bool {
 		}
 	}
 	return false
+}
+
+func defaultTweakListOptions() cache.TweakListOptionsFunc {
+	globalLabelSelector := os.Getenv("CATTLE_GLOBAL_LABEL_SELECTOR")
+	if globalLabelSelector == "" {
+		return func(*v1.ListOptions) {}
+	}
+	return func(opts *v1.ListOptions) {
+		opts.LabelSelector = globalLabelSelector
+	}
 }
 
 // WebhookImpersonation returns a ImpersonationConfig that can be used for impersonating the webhook's sudo account and bypass validation.
